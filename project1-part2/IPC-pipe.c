e5f7264f3d39ce25801dae1775b921c1b346d80b
avzaman <vince@avzaman.dev>
2026-03-05 13:51:22 -0700

    first edits to pipe func sorta

--- project1-part2/IPC-pipe.c
+++ project1-part2/IPC-pipe.c
@@ -14,7 +14,8 @@
 
 //Add all your global variables and definitions here.
 #define MATRIX_SIZE 1000
-
+#define RANDOM_MAXIMUM 10
+#define DEBUG_SWITCH 1
 
 void semaphore_init(int sem_id, int sem_num, int init_valve)
 {
@@ -60,13 +61,77 @@ void print_stats(int matrix_size, int num_processes, int verified, double elapse
     printf("Total runtime: %.6f seconds\n", elapsed);
 }
 
+void printMatrix(int m, int M[m][m]){
+	printf("Printing Matrix:\n");
+	for(int i = 0; i < m; i++){
+		for(int j = 0; j < m; j++){
+			printf("%4d ",M[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+int vector_mult(m,col,u[m],B[m][m]){
+	int sum = 0;
+	for(int i = 0; i < m; i++){
+		sum += u[i]*B[i][col];
+	}
+	return sum;
+}
+
 int main(int argc, char const *argv[])
 {
+	int m, num_procs, verified;
+	double elapsed;
 
+	if(argc > 1){
+		m = atoi(argv[1]);
+	}else{
+		m = MATRIX_SIZE;
+	}
 
-   /* Your completed code must uncomment, and call the below function.*/ 
-   //print_stats(MATRIX_SIZE, num_procs, verified, elapsed);
-   return 0;
+	// make two matrices to multiply together
+	int A[m][m], B[m][m];
+	for(int i = 0; i < m; i++){
+		for(int j = 0; j < m; j++){
+			A[i][j] = rand()%RANDOM_MAXIMUM;
+			B[i][j] = rand()%RANDOM_MAXIMUM;
+		}
+	}
+
+	if(DEBUG_SWITCH){
+		printMatrix(m,A);
+		printMatrix(m,B);
+	}
+
+	// initialize result matrix, create pipe, fork() for each row of A column of B, 
+	// child calculates, waits for semaphore, writes (int i, int j, int result) to pipe up aquiring lock
+	int C[m][m];
+	pid_t procID;
+	int pipe[2] = pipe();
+	int semaphoreID = 1;
+	init_semaphore(semaphoreID);
+	int res = 0;
+	for(int i = 0; i < m; i++){
+		for(int j = 0; j < m; j++){
+			procID = fork();
+			if(procID == 0){
+				res = vector_mult(m,j,A[i],B);
+				semaphore_reserve();
+				write(pipe[1], i, sizeof(int));
+				write(pipe[1], j, sizeof(int));
+				write(pipe[1], res, sizeof(int));
+				semaphore_release();
+				exit(0);
+			}
+		}
+	}
+
+	// parent process reads pipe 12 bytes at a time format i, j, result
+
+	/* Your completed code must uncomment, and call the below function.*/ 
+	print_stats(m, num_procs, verified, elapsed);
+	return 0;
 }
 
 
