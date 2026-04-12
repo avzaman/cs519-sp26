41d8263d0f290f78f63f34d4ff821db08a43a7c6
avzaman <avzaman@node-0.project2.rise-pg0.utah.cloudlab.us>
2026-04-12 04:32:08 +0000

    second compile

--- project2/compile_os_quick.sh
+++ project2/compile_os_quick.sh
@@ -15,7 +15,7 @@ scripts/config --disable SYSTEM_REVOCATION_KEYS
 touch REPORTING-BUGS
 #sudo make clean -j
 #sudo make prepare
-sudo make -j$PROC
+sudo make -j$PROC 2>&1 | tee build.log
 #sudo make modules -j$PROC
 sudo make modules_install
 sudo make install
